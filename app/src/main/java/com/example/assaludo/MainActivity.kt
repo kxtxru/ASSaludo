2d38d18e6ef2005113605c6f24e89c0d555268ba
alvagumar <alvagumar@gva.alu.edu.es>
2024-10-16 11:57:27 +0200

    1.2

---
+++ app/src/main/java/com/example/assaludo/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.assaludo
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
+        val etName:AppCompatEditText = findViewById(R.id.etname)
+        btnHello.setOnClickListener {
+            val name = etName.text.toString()
+            Toast.makeText(this, "Hola $name!",
+            Toast.LENGTH_LONG).show()
+
+
+        }
+    }
+}
