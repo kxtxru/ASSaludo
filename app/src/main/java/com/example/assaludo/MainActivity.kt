d60a1d4d016cc3923433055f50157c092e52294c
alvagumar <alvagumar@gva.alu.edu.es>
2024-10-23 09:40:27 +0200

    1.3

--- app/src/main/java/com/example/assaludo/MainActivity.kt
+++ app/src/main/java/com/example/assaludo/MainActivity.kt
@@ -1,27 +1,26 @@
 package com.example.assaludo
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val btnHello:AppCompatButton = findViewById(R.id.btnHello)
-        val etName:AppCompatEditText = findViewById(R.id.etname)
+        val btnHello: AppCompatButton = findViewById(R.id.btnHello)
+        val etName: AppCompatEditText = findViewById(R.id.etname)
         btnHello.setOnClickListener {
             val name = etName.text.toString()
-            Toast.makeText(this, "Hola $name!",
-            Toast.LENGTH_LONG).show()
-
-
+            val intentGA = Intent(this, MainActivity2::class.java)
+            intentGA.putExtra("EXTRA_NAME", name)
+            startActivity(intentGA)
         }
     }
 }
